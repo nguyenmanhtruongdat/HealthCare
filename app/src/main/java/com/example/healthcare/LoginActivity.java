719caa71176b7e5b505d1002da749baf4eed39b1
nguyenmanhtruongdat <datnmt.21it@vku.udn.vn>
2023-04-02 20:02:32 +0700

    dat

--- app/src/main/java/com/example/healthcare/LoginActivity.java
+++ app/src/main/java/com/example/healthcare/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.example.healthcare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.healthcare.databinding.ActivityLoginBinding;
+
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
@@ -23,6 +25,12 @@ public class LoginActivity extends AppCompatActivity {
                 String username = binding.username.getText().toString();
                 String password = binding.password.getText().toString();
                 Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
