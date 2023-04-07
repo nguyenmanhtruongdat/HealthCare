be562e54496890ae91ab528991c067cee3ab41de
nguyenmanhtruongdat <datnmt.21it@vku.udn.vn>
2023-04-07 15:06:43 +0700

    dat

--- app/src/main/java/com/example/healthcare/HomePatientAcitivity.java
+++ app/src/main/java/com/example/healthcare/HomePatientAcitivity.java
@@ -1,14 +1,49 @@
 package com.example.healthcare;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomePatientAcitivity extends AppCompatActivity {
+import com.example.healthcare.databinding.ActivityHomePatientAcitivityBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class HomePatientAcitivity extends AppCompatActivity {
+private ActivityHomePatientAcitivityBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_patient_acitivity);
+        binding = ActivityHomePatientAcitivityBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = currentUser.getUid();
+
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+        usersRef.child(userId).child("email").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String userEmail = dataSnapshot.getValue(String.class);
+                // Display the welcome message
+                String welcomeMessage = "Welcome, " + userEmail + "!";
+                binding.welcome.setText("Welcome"+ userEmail);
+                Toast.makeText(HomePatientAcitivity.this, welcomeMessage, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle the error
+            }
+        });
+
+
     }
 }
